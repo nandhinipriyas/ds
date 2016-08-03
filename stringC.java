ea6fee3c0f55d44a2874f4afa7e1c54fd487b366
nandhinipriyas <npriya163@gmail.com>
2016-08-03 04:18:11 -0700

    ikuk
    jhk

---
+++ stringC.java
@@ -0,0 +1,14 @@
+package activity;
+
+import java.util.Scanner;
+
+public class Sum {
+	
+		 public static void main(String Args[])
+		 {int num =Integer.parseInt("95");
+		 double c = Double.parseDouble("55");
+		 int a = Integer.parseInt("323",5);
+		       System.out.println(num);
+		       System.out.println(c);
+		       System.out.println(a);
+		    }}
