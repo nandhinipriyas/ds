fe2609ac78e11c888e3f4c0262c90ad112d3fa03
nandhinipriyas <npriya163@gmail.com>
2016-08-03 04:20:11 -0700

    jtyu
    ikyui

---
+++ facc.java
@@ -0,0 +1,19 @@
+import java.io.InputStream;
+import java.util.Scanner;
+
+public class Factorial {
+public static void main(String[] args){
+	int num1,num;
+	
+	Scanner fac= new Scanner(System.in);
+	 num=fac.nextInt();
+	 num1=num;
+	for(int i = num;i>=;i--){
+		num1=num*i;
+	}
+	System.out.println("factorial:"+num1);
+}
+
+
+
+}
