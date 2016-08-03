e9430f7d67dd9cbeaaeea6237262796a2a9ea593
nandhinipriyas <npriya163@gmail.com>
2016-08-03 03:48:09 -0700

    swswd
    sddw

---
+++ ascii.java
@@ -0,0 +1,12 @@
+package activity;
+
+import java.util.Scanner;
+
+public class Ascii {
+public static void main(String[] args) {
+
+    for(char letter= ' ';letter<274;letter++)
+       System.out.print(letter);
+}
+}
+}
